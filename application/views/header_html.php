88caf981ab80071cce71e7ac26376daf5c75f012
summakarn <summakarn@gmail.com>
2020-06-13 02:14:17 +0700

    [Add] View Section

---
+++ application/views/header_html.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<?php echo '<html lang="en">'; ?>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Cs Project Storage System</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/asset/css/bootstrap.min.css">
+    
+</head>
