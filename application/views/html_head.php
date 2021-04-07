28e1683bbbebb9c43edea7fae7aadb9ae958a200
NiShiJi Naito <46283851+summakarn@users.noreply.github.com>
2021-04-07 11:47:03 +0700

    [Edit] change background

--- application/views/html_head.php
+++ application/views/html_head.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
     <div class="row justify-content-center">
         <div class="col-md-11">
-            <div class="jumbotron jumbotron-fluid" style="background-color: #66ff66;">
+            <div class="jumbotron jumbotron-fluid" >
                 <div class="container">
                     <h3 class="display-4">ระบบสารสนเทศ</h3>
                     <p class="lead">ฝึกงานฤดูร้อน/สหกิจศึกษา</p>
